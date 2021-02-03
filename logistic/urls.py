d258486f5d689bbab7bbfd4f0cca14862eb1cfe4
Alex Khlybov <halexx7@gmail.com>
2021-02-03 02:25:28 +0000

    Lesson 2. Templates

--- logistic/urls.py
+++ logistic/urls.py
@@ -5,10 +5,10 @@ import mainapp.views as mainapp
 
 urlpatterns =[
     path("admin/", admin.site.urls),
-    path("", mainapp.main),
-    path("blog/", mainapp.blog),
-    path("text/", mainapp.text),
-    path("text2/", mainapp.text2),
-    path("services/", mainapp.services),
-    path("contact/", mainapp.contact),
+    path("", mainapp.main, name='main'),
+    path("blog/", mainapp.blog, name='blog'),
+    path("text/", mainapp.text, name='text'),
+    path("text2/", mainapp.text2, name='text2'),
+    path("services/", mainapp.services, name='services'),
+    path("contact/", mainapp.contact, name='contact'),
 ]
