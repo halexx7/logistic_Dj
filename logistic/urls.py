0bef54c46c55cf5de764a92fb87942950dc78dc2
Alex Khlybov <halexx7@gmail.com>
2021-02-06 19:54:56 +0000

    Lesson 3. Django ORM start

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -15,3 +17,6 @@ urlpatterns =[
     path("services/air", mainapp.services, name='services_air'),
     path("contact/", mainapp.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,60 @@
+from os import name
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class ServicesCategory(models.Model):
+    name = models.CharField(verbose_name="name_category", max_length=32, unique=True)
+    name_long = models.CharField(verbose_name="name_long", max_length=64)
+    desc_category = models.TextField(verbose_name="description")
+
+    def __str__(self):
+        return self.name
+
+
+class Services(models.Model):
+    category = models.ForeignKey(ServicesCategory, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="services", max_length=128)
+    image = models.ImageField(upload_to="services_images", blank=True)
+    description = models.TextField(verbose_name="description", blank=True)
+    lifting = models.CharField(verbose_name="lifting", max_length=32) 
+    price = models.DecimalField(verbose_name="price", max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField(verbose_name="quantity", default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
+
+
+class News(models.Model):
+    title = models.CharField(verbose_name="title_news", max_length=256)
+    text = models.TextField(verbose_name="text_news")
+    public_date = models.DateTimeField(False, default=timezone.now)
+
+    def __str__(self):
+        return f"{self.title} ({self.public_date})"
+
+
+
+class Benefits(models.Model):
+    title = models.CharField(verbose_name="name", max_length=128)
+    desc = models.TextField(verbose_name="description", blank=True)
+
+
+
+class Team(models.Model):
+    full_name = models.CharField(verbose_name="full_name", max_length=256)
+    profession = models.CharField(verbose_name="profession", max_length=256)
+    photo = models.ImageField(upload_to="team_photo", blank=True)
+    email = models.CharField(verbose_name="e-mail", max_length=128)
+    phone = models.CharField(verbose_name="phone", max_length=20)
+
+
+
+class Contacts(models.Model):
+    city = models.CharField(verbose_name="city", max_length=256)
+    phone = models.CharField(verbose_name="phone", max_length=20)
+    email = models.CharField(verbose_name="e-mail", max_length=128)
+    address = models.CharField(verbose_name="address", max_length=512)
+
+    
+
