1cd6c1db96f2c404d781306a180fb04e558d7616
Alexey Khlybov <halexx7@gmail.com>
2021-02-08 01:20:55 +0000

    Lesson 3. Djagno admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Services, ServicesCategory, News, Benefits, Team, Contacts
+
+admin.site.register(ServicesCategory)
+admin.site.register(Services)
+admin.site.register(News)
+admin.site.register(Benefits)
+admin.site.register(Team)
+admin.site.register(Contacts)
