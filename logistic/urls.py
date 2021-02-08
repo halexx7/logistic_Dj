b0e29f09fc7aa67cc23b0a61cf42faa727817595
Alexey Khlybov <halexx7@gmail.com>
2021-02-08 02:12:18 +0000

    Lesson 3. Urls for app

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
+from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+
 import mainapp.views as mainapp
 
 urlpatterns =[
@@ -10,10 +12,7 @@ urlpatterns =[
     path("blog/", mainapp.blog, name='blog'),
     path("text/", mainapp.text, name='text'),
     path("text2/", mainapp.text2, name='text2'),
-    path("services/", mainapp.services, name='services'),
-    path("services/railway", mainapp.services, name='services_railway'),
-    path("services/water", mainapp.services, name='services_water'),
-    path("services/air", mainapp.services, name='services_air'),
+    path("services/", include("mainapp.urls", namespace="services")),
     path("contact/", mainapp.contact, name='contact'),
 ]
 
