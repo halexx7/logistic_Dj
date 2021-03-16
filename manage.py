c16b9fc742d90cbf777e8cb4cb5dc50b24421728
halexx7 <63438275+halexx7@users.noreply.github.com>
2021-03-16 04:47:23 +0300

    Lesson 11. Dropdown and Ordersapp (#11)
    * lesson_11. Dropdown
    
    * lesson_11. UPD button
    
    * lesson_11. Ordersapp
    
    * lesson_11. Setting up the debugger inside the container

--- manage.py
+++ manage.py
@@ -6,6 +6,18 @@ import sys
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "logistic.settings")
+
+    # start new section
+    from django.conf import settings
+
+    if settings.DEBUG:
+        if os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+            import ptvsd
+
+            ptvsd.enable_attach(address=('0.0.0.0', 3000))
+            print('Attached!')
+    # end new section
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +29,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
