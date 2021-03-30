6fa3ef34be9cf0985d558c298c57b68c5d63ca7b
halexx7 <halexx7@gmail.com>
2021-03-30 17:01:03 +0300

    lesson_16. F objects

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
+from django.db.models import F
 from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
@@ -130,18 +131,16 @@ def order_forming_complete(request, pk):
 @receiver(pre_save, sender=Basket)
 def product_quantity_update_save(instance, sender, **kwargs):
     if instance.pk:
-        """If user change quantity in order or basket"""
-        instance.service.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        instance.service.quantity = F("quantity") - (instance.quantity - sender.get_item(instance.pk).quantity)
     else:
-        """If user create order or basket"""
-        instance.service.quantity -= instance.quantity
+        instance.service.quantity = F("quantity") - instance.quantity
     instance.service.save()
 
 
 @receiver(pre_delete, sender=OrderItem)
 @receiver(pre_delete, sender=Basket)
 def product_quantity_update_delete(instance, **kwargs):
-    instance.service.quantity += instance.quantity
+    instance.service.quantity = F("quantity") + instance.quantity
     instance.service.save()
 
 
